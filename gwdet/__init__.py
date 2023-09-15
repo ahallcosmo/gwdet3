028e8d748193274b31379f63fedeb814da0700d2
Alex Hall <ach740@gmail.com>
2023-09-15 10:48:40 +0100

    add spec to init

--- gwdet/__init__.py
+++ gwdet/__init__.py
@@ -1,3 +1,9 @@
+from .gwdet import __author__
+from .gwdet import __email__
+from .gwdet import __license__
+from .gwdet import __version__
+from .gwdet import __doc__
+
 from .gwdet import averageangles
 from .gwdet import detectability
 from .gwdet import compare_Pw
